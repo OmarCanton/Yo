5916f2e8fb397de3c2fa28670f07f4a6d2522971
OmarCanton <abdulaiumar20@gmail.com>
2025-09-20 00:02:11 +0000

    commit

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -493,7 +493,7 @@ export default function Home() {
                             <motion.span     
                                 initial={{y: '10%', opacity: 0}}
                                 animate={{y: 0, opacity: 1, transition: {
-                                    delay: 2.5
+                                    delay: 0.5
                                 }}}
                                 exit={{y: '10%', opacity: 0}}
                             >
